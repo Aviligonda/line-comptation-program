f7d3896b4d098d35b96abaa87c8945a953d79f6d
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-06-13 10:30:11 +0530

    Added :check equality of two lines

--- LineComputation.java
+++ LineComputation.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class LineComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Computation program");
-        double lengthOfLine;
+        double lengthOfLine, line1, line2;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the x1 value");
         double x1 = scanner.nextDouble();
@@ -15,5 +15,14 @@ public class LineComputation {
         double y2 = scanner.nextDouble();
         lengthOfLine = Math.sqrt(Math.pow((x2 - x1), 2 + Math.pow((y2 - y1), 2)));
         System.out.println("Length of line is :" + lengthOfLine);
+        line1 = x2 - x1;
+        System.out.println(line1);
+        line2 = y2 - y1;
+        System.out.println(line2);
+        if (line1 == line2) {
+            System.out.println("Two lines are equal");
+        } else {
+            System.out.println("Two lines are not equal");
+        }
     }
 }
