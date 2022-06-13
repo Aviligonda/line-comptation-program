0e887bfeaa8d4f6c5b42d6f7c3097e90ff258513
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-06-13 09:52:52 +0530

    Added : Finding lenth of line value

--- LineComputation.java
+++ LineComputation.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class LineComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Computation program");
+        double lengthOfLine;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the x1 value");
+        double x1 = scanner.nextDouble();
+        System.out.println("Enter the x2 value");
+        double x2 = scanner.nextDouble();
+        System.out.println("Enter the y1 value");
+        double y1 = scanner.nextDouble();
+        System.out.println("Enter the y2 value");
+        double y2 = scanner.nextDouble();
+        lengthOfLine = Math.sqrt(Math.pow((x2 - x1), 2 + Math.pow((y2 - y1), 2)));
+        System.out.println("Length of line is :" + lengthOfLine);
     }
 }
